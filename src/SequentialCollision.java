417be028d110b11348bee53265178425b08e492b
Jeremiah Hanson <hanson.jeremiah@comcast.net>
2018-03-15 14:04:54 -0700

    eclipse project start and first java class

---
+++ src/SequentialCollision.java
@@ -0,0 +1,14 @@
+/*
+ * SequentialCollision.java
+ * Author: Jeremiah Hanson
+ * ---------------------------------------------------------------
+ * This is an n-body collision program that runs sequentially with
+ * one process.
+ */
+
+public class SequentialCollision {
+
+	public static void main(String[] args) {
+		
+	}
+}
